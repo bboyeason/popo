c3de57ec47cc4c2c74fc7f74b85144823218563d
easonlh_cheng <easonlh_cheng@compal.com>
2015-10-31 16:11:07 +0800

    Remove unnecessary code

--- app/src/main/java/com/nosae/game/objects/Quiz.java
+++ app/src/main/java/com/nosae/game/objects/Quiz.java
@@ -20,8 +20,8 @@ public class Quiz extends GameObj{
     private quizSyllable[] syllables = {quizSyllable.Do, quizSyllable.Re, quizSyllable.Mi, quizSyllable.Fa, quizSyllable.So};
 
     private QuizItem quizTable[];
-    private int currentQuiz;
-    private int oldQuiz;
+    private static int currentQuiz;
+    private static int oldQuiz;
     public static boolean isQuizHit = false;
 
     public int width = 0;
@@ -31,13 +31,6 @@ public class Quiz extends GameObj{
 
     private int col = 5;
     private static Random mRandom;
-
-/*    private enum quizTable {
-        redDo, redRe, redMi, redFa, redSo,
-        yellowDo, yellowRe, yellowMi, yellowFa, yellowSo,
-        blueDo, blueRe, blueMi, blueFa, blueSo
-    }*/
-
 
     public Quiz(int destX, int destY, int destWidth, int destHeight, int srcX, int srcY, int srcWidth, int srcHeight, int speed, int color, int theta) {
         super(destX, destY, destWidth, destHeight, srcX, srcY, srcWidth, srcHeight, speed, color, theta);
@@ -56,14 +49,6 @@ public class Quiz extends GameObj{
             quizTable[n].syllable = syllables[n % 5];
 //            DebugConfig.d("quizTable[" + n + "]: " + quizTable[n].color + ", " + quizTable[n].syllable);
         }
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; i < 5; j++) {
-//                quizTable[0].color = quizColor.red;
-//                quizTable[0].syllable = quizSyllable.Do;
-//                quizTable[1].color = quizColor.red;
-//                quizTable[1].syllable = quizSyllable.Re;
-//            }
-//        }
     }
 
     public void randomQuiz() {
