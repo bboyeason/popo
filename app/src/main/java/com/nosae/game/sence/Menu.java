10aee6c613b6599002d7370e6c9c2340e58a3bc1
easonlh_cheng <easonlh_cheng@compal.com>
2015-11-12 11:19:14 +0800

    [Bobo][Menu] Add logo animation
    Feature/Bug number:
        None
    
    Modifications (list dependencies if any):
    
    Side effect of the change:
        None

--- app/src/main/java/com/nosae/game/sence/Menu.java
+++ app/src/main/java/com/nosae/game/sence/Menu.java
@@ -7,14 +7,20 @@ import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.nosae.game.bobo.GameParams;
-import com.nosae.game.bobo.MainActivity;
 import com.nosae.game.bobo.R;
 
 import com.nosae.game.bobo.SplashScreen;
 import com.nosae.game.objects.Music;
+import com.nosae.game.settings.DebugConfig;
 
 /**
  * Created by eason on 2015/10/27.
@@ -22,6 +28,13 @@ import com.nosae.game.objects.Music;
 public class Menu extends Activity {
 
     private Music mMusic;
+    private ImageView mImageView;
+    private Button mSettingsButton;
+    private Button mKarnofskyScaleButton;
+    private Button mStartButton;
+    private Button mLoadButton;
+    private Button mExitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,12 +46,11 @@ public class Menu extends Activity {
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         GameParams.getScreenInfo(dm);
 
-        Button mSettingsButton = (Button) findViewById(R.id.settingsButton);
-        Button mKarnofskyScaleButton = (Button) findViewById(R.id.karnofskyButton);
-        Button mStartButton = (Button) findViewById(R.id.startButton);
-        Button mLoadButton = (Button) findViewById(R.id.loadButton);
-        Button mExitButton = (Button) findViewById(R.id.exitButton);
-//        mMusic = new Music(this, R.raw.menu, 1);
+        mSettingsButton = (Button) findViewById(R.id.settingsButton);
+        mKarnofskyScaleButton = (Button) findViewById(R.id.karnofskyButton);
+        mStartButton = (Button) findViewById(R.id.startButton);
+        mLoadButton = (Button) findViewById(R.id.loadButton);
+        mExitButton = (Button) findViewById(R.id.exitButton);
         if (mMusic == null) {
 //            mMusic.player.release();
             mMusic = new Music(this, R.raw.menu,2);
@@ -131,6 +143,40 @@ public class Menu extends Activity {
 
             }
         });
+
+        logoInit();
+    }
+
+    private void logoInit() {
+        DebugConfig.d("Menu logo init");
+        mImageView = new ImageView(this);
+        mImageView.setBackgroundResource(R.drawable.logo_1);
+        float fromX, toX, fromY, toY;
+        fromX = GameParams.scaleWidth;
+        toX = -GameParams.scaleWidth;
+        fromY = GameParams.scaleHeight - mImageView.getBackground().getIntrinsicHeight();
+        toY = fromY;
+        // Move effect
+        Animation amTranslate = new TranslateAnimation(fromX, toX, fromY, toY);
+        amTranslate.setDuration(6000);
+        amTranslate.setRepeatCount(-1);
+
+        // Transparent effect
+        Animation amAlpha = new AlphaAnimation(0.3f, 1.0f);
+        amAlpha.setDuration(6000);
+//        amAlpha.setRepeatCount(-1);
+
+        // Animation effect combine
+        AnimationSet amSet = new AnimationSet(false);
+        amSet.addAnimation(amTranslate);
+        amSet.addAnimation(amAlpha);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.menu_root);
+        layout.addView(mImageView, 0);
+        mImageView.startAnimation(amSet);
+
+        mSettingsButton.startAnimation(amAlpha);
+        mExitButton.startAnimation(amAlpha);
     }
 
     @Override
