0c394fcfe0cd6d4829583da5f50fc7f681d298b6
easonlh_cheng <easonlh_cheng@compal.com>
2015-11-12 09:53:00 +0800

    [Bobo][Stage 1] Update new sea star image
    Feature/Bug number:
        None
    
    Modifications (list dependencies if any):
    
    Side effect of the change:
        None

--- app/src/main/java/com/nosae/game/sence/Stage1.java
+++ app/src/main/java/com/nosae/game/sence/Stage1.java
@@ -85,14 +85,14 @@ public class Stage1 extends DrawableGameComponent {
 
     private int[][] mFishTable2 = {
             {
-                    R.drawable.a_add_time,
+                    R.drawable.sea_star,
                     R.drawable.a_add_life
             },
-            {  5,  5 }, /* Animation column */
-            {  2,  2 }, /* Animation row */
+            { 10,  5 }, /* Animation column */
+            {  3,  2 }, /* Animation row */
             {  0,  0 },  /* Max index */
             {  1,  1 }, /* Death animation start */
-            {  9,  9 }, /* Death animation end */
+            { 27,  9 }, /* Death animation end */
             {  0,  0 }, /* Touch Score */
             {  0,  0 }, /* Arrival Score */
             { 10,  0 }, /* Timer add (seconds) */
