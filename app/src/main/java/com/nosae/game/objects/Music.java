16d926a13b79412baeee3dd5dd866d5ad6a098c0
easonlh_cheng <easonlh_cheng@compal.com>
2015-11-16 13:17:59 +0800

    [Bobo][Music] Implement set volume for futher use
    Feature/Bug number:
        None
    
    Modifications (list dependencies if any):
    
    Side effect of the change:
        None

--- app/src/main/java/com/nosae/game/objects/Music.java
+++ app/src/main/java/com/nosae/game/objects/Music.java
@@ -15,7 +15,9 @@ import com.nosae.game.settings.DebugConfig;
  * Created by eason on 2015/10/27.
  */
 public class Music{
+    private static float volumeRatio;
     public MediaPlayer player;
+    public static int volume;
     public boolean isComplete;
     public static Context context;
 
@@ -23,9 +25,7 @@ public class Music{
         this.context = context;
         player = MediaPlayer.create(context, music);
 
-        // (0~10)
-        player.setVolume(volume, volume);
-
+        setVolume(volume);
         isComplete = false;
 
         player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -35,12 +35,27 @@ public class Music{
         });
     }
 
+    public void setVolume(int volume) {
+        // (0~10)
+        this.volume = volume;
+        player.setVolume(volume, volume);
+    }
+
+    public void setVolumeRatio(float volumeRatio) {
+        this.volumeRatio = volumeRatio;
+    }
+
     public static void soundPoolInit() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             createNewSoundPool();
         } else {
             createOldSoundPool();
         }
+
+        AudioManager audioManager = (AudioManager) context.getSystemService(context.AUDIO_SERVICE);
+        float audioMaxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        float audioCurrentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        volumeRatio = audioCurrentVolume/audioMaxVolume;
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -71,14 +86,11 @@ public class Music{
     public static void playSound(int sound, int number) {
         if (!GameParams.isSoundOn)
             return;
-        AudioManager am = (AudioManager) context.getSystemService(context.AUDIO_SERVICE);//实例化AudioManager对象
-        float audioMaxVolumn = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);//返回当前AudioManager对象的最大音量值
-        float audioCurrentVolumn = am.getStreamVolume(AudioManager.STREAM_MUSIC);//返回当前AudioManager对象的音量值
-        float volumnRatio = audioCurrentVolumn/audioMaxVolumn;
+
         GameParams.soundPool.play(
                 sound,          //播放的音乐id
-                volumnRatio,    //左声道音量
-                volumnRatio,    //右声道音量
+                volumeRatio,    //左声道音量
+                volumeRatio,    //右声道音量
                 1,              //优先级，0为最低
                 number,         //循环次数，0无不循环，-1无永远循环
                 1               //回放速度 ，该值在0.5-2.0之间，1为正常速度
@@ -111,152 +123,3 @@ public class Music{
         }
     }
 }
-
-
-
-//import java.util.HashMap;
-//		import java.util.Map;
-//		import java.util.Random;
-//		 
-//		import com.llkan.R;
-//		 
-//		import android.content.Context;
-//		import android.media.AudioManager;
-//		import android.media.MediaPlayer;
-//		import android.media.SoundPool;
-//		 
-///**
-//  * 声音控制类
-//  * @author wyf
-//  *
-//  */
-//public class SoundPlayer {
-//		 
-//		    private static MediaPlayer music;
-//		    private static SoundPool soundPool;
-//		     
-//		    private static boolean musicSt = true; //音乐开关
-//		    private static boolean soundSt = true; //音效开关
-//		    private static Context context;
-//		     
-//		    private static final int[] musicId = {R.raw.bg,R.raw.bg1,R.raw.bg2,R.raw.bg3};
-//		    private static Map<Integer,Integer> soundMap; //音效资源id与加载过后的音源id的映射关系表
-//		     
-//		    /**
-//      * 初始化方法
-//      * @param c
-//      */
-//		    public static void init(Context c)
-//		    {
-//		        context = c;
-//		 
-//		        initMusic();
-//		         
-//		        initSound();
-//		    }
-//		     
-//		    //初始化音效播放器
-//		    private static void initSound()
-//		    {
-//		        soundPool = new SoundPool(10,AudioManager.STREAM_MUSIC,100);
-//		         
-//		        soundMap = new HashMap<Integer,Integer>();
-//		        soundMap.put(R.raw.itemboom, soundPool.load(context, R.raw.itemboom, 1));
-//		        soundMap.put(R.raw.sel, soundPool.load(context, R.raw.sel, 1));
-//		    }
-//		     
-//		    //初始化音乐播放器
-//		    private static void initMusic()
-//		    {
-//		        int r = new Random().nextInt(musicId.length);
-//		        music = MediaPlayer.create(context,musicId[r]);
-//		        music.setLooping(true);
-//		    }
-//		     
-//		    /**
-//      * 播放音效
-//      * @param resId 音效资源id
-//      */
-//		    public static void playSound(int resId)
-//		    {
-//		        if(soundSt == false)
-//		            return;
-//		         
-//		        Integer soundId = soundMap.get(resId);
-//		        if(soundId != null)
-//		            soundPool.play(soundId, 1, 1, 1, 0, 1);
-//		    }
-//		 
-//		    /**
-//      * 暂停音乐
-//      */
-//		    public static void pauseMusic()
-//		    {
-//		        if(music.isPlaying())
-//		            music.pause();
-//		    }
-//		     
-//		    /**
-//      * 播放音乐
-//      */
-//		    public static void startMusic()
-//		    {
-//		        if(musicSt)
-//		            music.start();
-//		    }
-//		     
-//		    /**
-//      * 切换一首音乐并播放
-//      */
-//		    public static void changeAndPlayMusic()
-//		    {
-//		        if(music != null)
-//		            music.release();
-//		        initMusic();
-//		        startMusic();
-//		    }
-//		     
-//		    /**
-//      * 获得音乐开关状态
-//      * @return
-//      */
-//		    public static boolean isMusicSt() {
-//		        return musicSt;
-//		    }
-//		     
-//		    /**
-//      * 设置音乐开关
-//      * @param musicSt
-//      */
-//		    public static void setMusicSt(boolean musicSt) {
-//		        SoundPlayer.musicSt = musicSt;
-//		        if(musicSt)
-//		            music.start();
-//		        else
-//		            music.stop();
-//		    }
-//		 
-//		    /**
-//      * 获得音效开关状态
-//      * @return
-//      */
-//		    public static boolean isSoundSt() {
-//		        return soundSt;
-//		    }
-//		 
-//		    /**
-//      * 设置音效开关
-//      * @param soundSt
-//      */
-//		    public static void setSoundSt(boolean soundSt) {
-//		        SoundPlayer.soundSt = soundSt;
-//		    }
-//		     
-//		    /**
-//      * 发出‘邦’的声音
-//      */
-//		    public static void boom()
-//		    {
-//		        playSound(R.raw.itemboom);
-//		    }
-//		}
