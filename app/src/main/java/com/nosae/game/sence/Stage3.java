ab995524a29be3447442f83fcc50bc88a56cf683
easonlh_cheng <easonlh_cheng@compal.com>
2015-12-06 23:25:45 +0800

    [Bobo][Stage 3] Add stage 3 basic scene object
    Feature/Bug number:
        None
    
    Modifications (list dependencies if any):
    
    Side effect of the change:
        None

--- app/src/main/java/com/nosae/game/sence/Stage3.java
+++ app/src/main/java/com/nosae/game/sence/Stage3.java
@@ -1,10 +1,21 @@
 package com.nosae.game.sence;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+
 import com.nosae.game.bobo.GameEntry;
+import com.nosae.game.bobo.GameParams;
+import com.nosae.game.bobo.R;
+import com.nosae.game.bobo.Text;
+import com.nosae.game.objects.GameObj;
+import com.nosae.game.objects.Life1;
+import com.nosae.game.objects.Score;
+import com.nosae.game.objects.TimerBar2;
 import com.nosae.game.settings.DebugConfig;
 
 import lbs.DrawableGameComponent;
-import lbs.GameComponentCollection;
 
 /**
  * Created by eason on 2015/11/9.
@@ -12,10 +23,151 @@ import lbs.GameComponentCollection;
 public class Stage3 extends DrawableGameComponent {
     private final GameEntry mGameEntry;
 
+    private GameObj mBackground;
+    private Bitmap mBackGroundImage;
+
+    private GameObj mSceneTitle;
+    private Bitmap mSceneTitleImage;
+
+    private Score mScore;
+
+    private Text mFpsText;
+
+    public TimerBar2 mTimerBar;
+    public Bitmap mTimerBarImage;
+
+    private GameObj mLifeIcon;
+    private Bitmap mLifeImage;
+    private Life1 mLife1;
+
+    // TODO move below to GameParams class
+    public static boolean isClearStage3 = false;
+    public static int mTotalScore = 0;
+    public static boolean isGameOver = false;
+
+
     public Stage3(GameEntry gameEntry) {
         DebugConfig.d("Stage3 Constructor");
         this.mGameEntry = gameEntry;
     }
 
+    @Override
+    protected void Initialize() {
+        super.Initialize();
+    }
 
+    @Override
+    protected void LoadContent() {
+        super.LoadContent();
+        DebugConfig.d("Stage3 LoadContent()");
+        int width, height;
+
+        if (mBackground == null) {
+            mBackGroundImage = GameParams.decodeSampledBitmapFromResource(R.drawable.a_background, GameParams.scaleWidth, GameParams.scaleHeight);
+            mBackground = new GameObj(0, 0, GameParams.scaleWidth, GameParams.scaleHeight, 0, 0, mBackGroundImage.getWidth(), mBackGroundImage.getHeight(), 0, 0, 0);
+            mBackground.isAlive = true;
+        }
+
+        if (mSceneTitle == null) {
+            mSceneTitleImage = (Bitmap) BitmapFactory.decodeResource(GameParams.res, R.drawable.a_stage_title);
+            mSceneTitle = new GameObj(GameParams.halfWidth, (int) (10 * GameParams.density), mSceneTitleImage.getWidth(), mSceneTitleImage.getHeight(), 0, 0, mSceneTitleImage.getWidth(), mSceneTitleImage.getHeight(), 0, 0, 0);
+        }
+
+        if (DebugConfig.isFpsDebugOn) {
+            mFpsText = new Text(GameParams.halfWidth - 50, 100, 12, "FPS", Color.BLUE);
+        }
+
+        if (mScore == null)
+            mScore = new Score((int) (20 * GameParams.density), (int) (20 * GameParams.density));
+
+        if (mLifeIcon == null) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 4;
+            mLifeImage = (Bitmap) BitmapFactory.decodeResource(GameParams.res, R.drawable.b_life, options);
+            mLifeIcon = new GameObj(mScore.destRect.left, mScore.getY() + mScore.height + (int) (5 * GameParams.density), mLifeImage.getWidth(), mLifeImage.getHeight(), 0, 0, mLifeImage.getWidth(), mLifeImage.getHeight(), 0, 0, 0);
+        }
+
+        if (mLife1 == null) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 2;
+            Bitmap numBitmap = (Bitmap) BitmapFactory.decodeResource(GameParams.res, R.drawable.s_0, options);
+            mLife1 = new Life1(mLifeIcon.destRect.right + (int) (10 * GameParams.density), mLifeIcon.destRect.bottom - mLifeIcon.halfHeight - (numBitmap.getHeight() >> 1), numBitmap.getWidth(), numBitmap.getHeight(), 0, 0, numBitmap.getWidth() * 2, numBitmap.getHeight() * 2);
+            Life1.setLife(GameParams.stage3Life);
+            numBitmap.recycle();
+            numBitmap = null;
+        }
+
+        if (mTimerBar == null) {
+            mTimerBarImage = (Bitmap) BitmapFactory.decodeResource(GameParams.res, R.drawable.timer_bar);
+            if (mTimerBarImage != null) {
+                width = mTimerBarImage.getWidth() / 1;
+                height = mTimerBarImage.getHeight() / 9;
+                mTimerBar = new TimerBar2(mLife1.destRect.right + 10, mScore.getY() + (mLife1.destRect.bottom - mScore.getY()) / 2 - (height >> 1), width, height, 0, 0, width, height, 0, 0, 0);
+                if (mTimerBar != null)
+                    mTimerBar.setStartFrame((int) GameEntry.totalFrames);
+            }
+        }
+        if (mTimerBar != null) {
+            mTimerBar.setTimer(GameParams.stage3RunningTime);
+        }
+    }
+
+    @Override
+    protected void Update() {
+        super.Update();
+        if (DebugConfig.isFpsDebugOn) {
+            mFpsText.message = "actual FPS: " + (int) mGameEntry.actualFPS + " FPS (" + (int) mGameEntry.fps
+                    + ") " + (int) mGameEntry.totalFrames;
+        }
+        if (mScore != null)
+            mScore.setTotalScore(mTotalScore);
+
+        if (mLife1 != null) {
+            mLife1.updateLife();
+            mLife1.action();
+            if (Life1.getLife() <= 0)
+                isGameOver = true;
+        }
+
+        if (mTimerBar != null) {
+            mTimerBar.action((int) GameEntry.totalFrames);
+            if (mTimerBar.isTimeout)
+                isGameOver = true;
+        }
+
+    }
+
+    @Override
+    protected void Draw() {
+        super.Draw();
+        Canvas mSubCanvas = mGameEntry.canvas;
+        if (mBackground.isAlive)
+        {
+            mSubCanvas.drawBitmap(mBackGroundImage, mBackground.srcRect, mBackground.destRect, null);
+        }
+
+        if (mSceneTitle != null) {
+            mSubCanvas.drawBitmap(mSceneTitleImage, mSceneTitle.srcRect, mSceneTitle.destRect, null);
+        }
+
+
+        if (DebugConfig.isFpsDebugOn) {
+            mSubCanvas.drawText(mFpsText.message, mFpsText.x, mFpsText.y, mFpsText.paint);
+        }
+
+        if (mScore != null)
+            mScore.drawScore(mSubCanvas);
+
+        if (mLifeIcon != null) {
+            mSubCanvas.drawBitmap(mLifeImage, mLifeIcon.srcRect, mLifeIcon.destRect, null);
+        }
+
+        if (mLife1 != null) {
+            mLife1.draw(mSubCanvas);
+        }
+
+        if (mTimerBar != null) {
+            mSubCanvas.drawBitmap(mTimerBarImage, mTimerBar.srcRect, mTimerBar.destRect, null);
+        }
+    }
 }
