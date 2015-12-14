d719e48caff2b207fa32d0a32e8d038b5e41e9e9
easonlh_cheng <easonlh_cheng@compal.com>
2015-12-14 16:39:44 +0800

    [Popo] Add stage complete color mask effect
    Feature/Bug number:
        None
    
    Modifications (list dependencies if any):
    
    Side effect of the change:
        None

--- app/src/main/java/com/nosae/game/objects/ColorMask.java
+++ app/src/main/java/com/nosae/game/objects/ColorMask.java
@@ -8,77 +8,84 @@ import com.nosae.game.popo.Text;
 /**
  * Created by eason on 2015/10/27.
  */
-public class ColorMask extends GameObj{
+public class ColorMask extends GameObj {
 
-        private int startFrame;
-        private int delayFrame = 300;
-        public Text text;
-        private final static String STRING = "Game Over";
+    private int startFrame;
+    private int delayFrame = 300;
+    public Text text;
+    private boolean breakStage = false;
+    private final static String STRING = "Game Over";
 
-        public ColorMask(int color,int alpha) {
-            super(0, 0, GameParams.scaleWidth, GameParams.scaleHeight, color, alpha);
+    public ColorMask(int color, int alpha) {
+        super(0, 0, GameParams.scaleWidth, GameParams.scaleHeight, color, alpha);
 
-            text = new Text(GameParams.halfWidth - 100, GameParams.halfHeight, 36, STRING, Color.BLACK);
-        }
+        text = new Text(GameParams.halfWidth - 100, GameParams.halfHeight, 36, STRING, Color.BLACK);
+    }
 
-        public ColorMask(int x, int y, int destWidth, int destHeight, int color,int alpha) {
-            super(x, y, destWidth, destHeight, color, alpha);
-        }
+    public ColorMask(int color, int alpha, boolean breakStage) {
+        super(0, 0, GameParams.scaleWidth, GameParams.scaleHeight, color, alpha);
+        this.breakStage = breakStage;
+    }
 
-        public void Action(int frameTime)
-        {
-            switch(state)
-            {
-                case step1:
-                    if (!isAlive)
-                        isAlive = true;
-                    startFrame = frameTime;
+    public ColorMask(int x, int y, int destWidth, int destHeight, int color, int alpha) {
+        super(x, y, destWidth, destHeight, color, alpha);
+    }
+
+    public boolean Action(int frameTime) {
+        switch (state) {
+            case step1:
+                if (!isAlive)
+                    isAlive = true;
+                startFrame = frameTime;
+                state = State.step2;
+
+                break;
+            case step2:
+                if (breakStage)
+                    return FadeIn(2, 255);
+                else if (FadeIn())
+                    state = State.step3;
+
+                break;
+            case step3:
+                if (FadeOut())
                     state = State.step2;
 
-                    break;
-                case step2:
-                    if (FadeIn())
-                        state = State.step3;
-
-                    break;
-                case step3:
-                    if (FadeOut())
-                        state = State.step2;
-
-                    if (frameTime - startFrame > delayFrame)
-                    {
-                        // always display
+                if (frameTime - startFrame > delayFrame) {
+                    // always display
 //                        isAlive = false;
-                    }
+                    return true;
+                }
 
-                    break;
-            }
+                break;
         }
-
-        public boolean FadeIn()
-        {
-            alpha += 5;
-            if (alpha > 192)
-            {
-                alpha = 192;
-//                GameParams.playSound(Sound.alert);
-                return true;
-            }
-
-            paint.setAlpha(alpha);
-            return false;
-        }
-
-        public boolean FadeOut()
-        {
-            alpha -= 5;
-            if (alpha < 0)
-            {
-                alpha = 0;
-                return true;
-            }
-
-            paint.setAlpha(alpha);
-            return false;
-        }
+        return false;
     }
+
+    public boolean FadeIn() {
+        return FadeIn(5, 192);
+    }
+
+    public boolean FadeIn(int _alpha, int _bound) {
+        alpha += _alpha;
+        if (alpha > _bound) {
+            alpha = _bound;
+//                GameParams.playSound(Sound.alert);
+            return true;
+        }
+
+        paint.setAlpha(alpha);
+        return false;
+    }
+
+    public boolean FadeOut() {
+        alpha -= 5;
+        if (alpha < 0) {
+            alpha = 0;
+            return true;
+        }
+
+        paint.setAlpha(alpha);
+        return false;
+    }
+}
