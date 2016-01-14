6aad21313be7eef705c8a6fb8d1d341dbb3c0eaa
easonlh_cheng <easonlh_cheng@compal.com>
2016-01-14 11:47:16 +0800

    [Popo][Stage 4] Apply move to screen right over half of main role
    Feature/Bug number:
        None
    
    Modifications (list dependencies if any):
    
    Side effect of the change:
        None
    
    Reference:

--- app/src/main/java/com/nosae/game/scene/Stage4.java
+++ app/src/main/java/com/nosae/game/scene/Stage4.java
@@ -189,10 +189,10 @@ public class Stage4 extends DrawableGameComponent {
 //            DebugConfig.d("onSensorChanged: " + event.values[0]);
             if (mPopoObj != null) {
                 mPopoObj.addX((int) -event.values[SensorManager.DATA_X] << 2);
-                if (mPopoObj.getX() < GameParams.screenRect.left - (mPopoObj.srcWidth >> 1))
-                    mPopoObj.setX(GameParams.screenRect.left - (mPopoObj.srcWidth >> 1));
-                else if (mPopoObj.getX() + mPopoObj.destWidth > GameParams.screenRect.right)
-                    mPopoObj.setX(GameParams.screenRect.right - mPopoObj.destWidth);
+                if (mPopoObj.getX() < GameParams.screenRect.left - (mPopoObj.destWidth >> 1))
+                    mPopoObj.setX(GameParams.screenRect.left - (mPopoObj.destWidth >> 1));
+                else if (mPopoObj.getX() + (mPopoObj.destWidth >> 1) > GameParams.screenRect.right)
+                    mPopoObj.setX(GameParams.screenRect.right - (mPopoObj.destWidth >> 1));
             }
         }
 
