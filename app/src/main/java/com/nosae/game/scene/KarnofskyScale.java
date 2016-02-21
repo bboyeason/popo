2edf09cf4f757189f6d9da10546ce2824426d601
easonlh_cheng <easonlh_cheng@compal.com>
2016-02-21 23:19:12 +0800

    [Popo][Karnofsky] Add the 1st version of Karnofsky scale
    Feature/Bug number:
        None
    
    Modifications (list dependencies if any):
    
    Side effect of the change:
        None
    
    Reference:

--- app/src/main/java/com/nosae/game/scene/KarnofskyScale.java
+++ app/src/main/java/com/nosae/game/scene/KarnofskyScale.java
@@ -2,6 +2,8 @@ package com.nosae.game.scene;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.nosae.game.popo.R;
 
@@ -13,5 +15,17 @@ public class KarnofskyScale extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.karnofsky_scale);
+        ImageView imageViewInfo = (ImageView) findViewById(R.id.imageViewInfo);
+        try {
+            imageViewInfo.setImageResource(R.drawable.k_introduction);
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+        }
+        imageViewInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
