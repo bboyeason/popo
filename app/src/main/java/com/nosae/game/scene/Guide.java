5f098c972d6c40268a1b932e0b3db0bd41cac381
easonlh_cheng <easonlh_cheng@compal.com>
2016-02-22 10:46:53 +0800

    [Popo][Guide] Implement guide animation
    Feature/Bug number:
        None
    
    Modifications (list dependencies if any):
        Need to support different dpi deivce
    
    Side effect of the change:
        None
    
    Reference:

--- app/src/main/java/com/nosae/game/scene/Guide.java
+++ app/src/main/java/com/nosae/game/scene/Guide.java
@@ -1,15 +1,22 @@
 package com.nosae.game.scene;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AnimationSet;
 import android.widget.ImageView;
 
 import com.nosae.game.popo.GameParams;
 import com.nosae.game.popo.MainActivity;
 import com.nosae.game.popo.R;
+import com.nosae.game.settings.DebugConfig;
 
 /**
  * Created by eason on 2015/10/27.
@@ -24,25 +31,58 @@ public class Guide extends Activity {
         setContentView(R.layout.guide);
         sInstance = this;
 
+        ObjectAnimator objAnimatorX;
+        ObjectAnimator objAnimatorY;
+        AnimatorSet animatorSet;
         ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        ImageView imageViewHand = (ImageView) findViewById(R.id.imageViewHand);
+
         Intent intent = getIntent();
         stage = intent.getIntExtra(GameParams.STAGE, 0);
         try {
             switch (stage) {
                 case 1:
                     imageView.setImageResource(R.drawable.guide01);
+                    imageViewHand.setImageResource(R.drawable.guide_hand);
+                    objAnimatorX = ObjectAnimator.ofFloat(imageViewHand, "translationX", imageViewHand.getX(), -100 * GameParams.densityDpi / 160);
+                    objAnimatorY = ObjectAnimator.ofFloat(imageViewHand, "translationY", imageViewHand.getY(), -40 * GameParams.densityDpi / 160);
+                    objAnimatorY.setInterpolator(new AccelerateDecelerateInterpolator());
+                    animatorSet = new AnimatorSet();
+                    animatorSet.play(objAnimatorX).with(objAnimatorY);
+                    animatorSet.setDuration(1500);
+                    animatorSet.start();
                     break;
                 case 2:
                     imageView.setImageResource(R.drawable.guide02);
+                    imageViewHand.setImageResource(R.drawable.guide_hand);
+                    objAnimatorX = ObjectAnimator.ofFloat(imageViewHand, "translationX", imageViewHand.getX(), -140 * GameParams.densityDpi / 160);
+                    objAnimatorY = ObjectAnimator.ofFloat(imageViewHand, "translationY", imageViewHand.getY(), -220 * GameParams.densityDpi / 160);
+                    objAnimatorY.setInterpolator(new AccelerateDecelerateInterpolator());
+                    animatorSet = new AnimatorSet();
+                    animatorSet.play(objAnimatorX).with(objAnimatorY);
+                    animatorSet.setDuration(1500);
+                    animatorSet.start();
                     break;
                 case 3:
                     imageView.setImageResource(R.drawable.guide03);
+                    imageViewHand.setImageResource(R.drawable.guide_hand);
+                    objAnimatorX = ObjectAnimator.ofFloat(imageViewHand, "translationX", imageViewHand.getX(), -60 * GameParams.densityDpi / 160);
+                    objAnimatorY = ObjectAnimator.ofFloat(imageViewHand, "translationY", imageViewHand.getY(), -190 * GameParams.densityDpi / 160);
+                    objAnimatorY.setInterpolator(new AccelerateDecelerateInterpolator());
+                    animatorSet = new AnimatorSet();
+                    animatorSet.play(objAnimatorX).with(objAnimatorY);
+                    animatorSet.setDuration(1500);
+                    animatorSet.start();
                     break;
                 case 4:
                     imageView.setImageResource(R.drawable.guide04);
+                    imageViewHand.setBackgroundResource(R.drawable.hand_shake_animation);
+                    ((AnimationDrawable) imageViewHand.getBackground()).start();
                     break;
                 case 5:
                     imageView.setImageResource(R.drawable.guide05);
+                    imageViewHand.setBackgroundResource(R.drawable.hand_shake_animation);
+                    ((AnimationDrawable) imageViewHand.getBackground()).start();
                     break;
             }
         } catch (OutOfMemoryError e) {
